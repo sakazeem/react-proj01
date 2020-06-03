3ec52a8af5e49e6b5db157bb2c9486e4d4b242e5
Salman Azeem <salmanazeemkhan@gmail.com>
2020-06-03 14:28:58 +0400

    text changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Update by Salman Azeem
+          File update by Salman Azeem
         </p>
         <a
           className="App-link"
